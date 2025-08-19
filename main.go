fab80de6e3bb9f1fe0fc6c816a9969f85c36e059
thom151 <thomassantos2003@gmail.com>
2025-08-19 22:40:02 +1000

    initial commit

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hello World")
-	return
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
+
