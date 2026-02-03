c6dc5483bb8608ebc90d85b88e521db4d251d30d
thom151 <thomassantos2003@gmail.com>
2026-02-03 13:55:31 +1100

    photo w/ motion

--- internal/heygen/heygen_functions.go
+++ internal/heygen/heygen_functions.go
@@ -24,8 +24,8 @@ func GenerateVideoHeygen(ctx context.Context, script, key, avatarId, voiceID, fi
 		VideoInputs: []VideoInput{
 			{
 				Character: &CharacterSettings{
-					Type:           "avatar",
-					AvatarID: avatarId,
+					Type:           "talking_photo",
+					TalkingPhotoID: avatarId,
 					Scale:          1.0,
 					AvatarStyle:    "normal",
 				},
